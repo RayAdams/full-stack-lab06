0ef27d298fa5f5c7a109eaf08be2d8f1e89e04fc
RayAdams <cramerrachael@gmail.com>
2017-06-06 18:36:23 -0400

    working model

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //5 friends array
-var friendArray = ['Sara', 'Dom', 'Hadlea', 'Pepper', 'Clint'];
+var friendArray = ['Sara', 'Sam', 'Hadlea', 'Pepper', 'Clint'];
 
 Array.prototype.random = function () {
   return this[Math.floor((Math.random()*this.length))];
@@ -7,37 +7,22 @@ Array.prototype.random = function () {
 //10 locations array
 var locationArray = ['kitchen pantry', 'root celler', 'cold freezer', 'parking lot', 'patio', 'solarium', 'master bath', 'tent', 'swimming pool', 'dining room'];
 
-
 //20 weapons array
 var weaponArray = ['stand mixer', 'steel whisk', 'bottle opener', 'corkscrew', 'butter knife', 'bread maker', 'icecream scoop', 'spoon', 'fork', 'spork','cookie sheet', 'wok', 'skillet', 'pizza cutter', 'veggie peeler', 'plastic wrap', 'butane torch', 'oven door', 'cayenne pepper', 'freezer lid'];
 
-
-
-//USING BIND:
-// create 100 h3 on page load
-for (var i = 1; i < 101; i++) {
-    var statement = document.createElement('h3');
-    statement.innerHTML = 'Accusation ' + i;
-    //set currentFriend to cycle through
-    //modulu by 5 using if to cycle through friendsArray
-    // currentFriend = friendArray[i];
-    // if (i % 5 === 0){
-    //     currentFriend = friendArray[i];
-    // }
-    
-    //add click eventListener to all h3
-    statement.addEventListener('click', function(){
-        alert('I accuse ' + currentFriend + ', with the ' +  weaponArray.random() + ' in the ' + locationArray.random() + '.');
-        }.bind(i));
-    document.body.appendChild(statement);
+//USING A Closure:
+function logFriend(i) {
+    return function(){
+        alert('I accuse ' + friendArray[i % 5]  + ', with the ' +  weaponArray.random() + ' in the ' + locationArray.random() + '.');
+    }
 }
 
-
-
-
-//readme notes
-// You will use the value of i in the loop to determine which of the above array items to use
-//     * Make sure you only select valid array indexes
-//     * Modulus is your friend
-//     * ex: 10 mod 10 is 0, 11 mod 10 is 1, 12 mod 10 is 2, ...
-
+// create 100 h3
+for (var i = 1; i <= 100; i++) {
+    var statement = document.createElement('h3');
+    statement.innerHTML = 'Accusation ' + i;
+    //create alert message
+    var friend = logFriend(i-1);
+    statement.addEventListener('click', friend);
+    document.body.appendChild(statement);
+}
